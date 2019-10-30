d8ddf9f597e71a9bf4116be8cf3f407f334cc35b
zeus <33592162+1nam@users.noreply.github.com>
2019-10-30 00:49:59 -0400

    Create the_calc.py

---
+++ the_calc.py
@@ -0,0 +1,13 @@
+# make an easy calculator using python.
+test1 = input("Type Your Number Here Please: ")
+test2 = input("Type Your Other Number Here Please: ")
+result1 = int(test1)
+result2 = int(test2)
+print("addition")
+print(result1 + result2)
+print("Multiplication")
+print(result1 * result2)
+print("Divided by")
+print(result1 % result2)
+print("Subtraction")
+print(result1 - result2)
